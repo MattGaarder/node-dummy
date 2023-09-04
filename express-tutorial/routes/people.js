ed468fcc229eabd2f92648a5c939a0df8c31fb9d
Matteus Gaarder <matteus.gaarder@gmail.com>
2023-09-04 13:12:18 +0100

    first part of tutorial completed, controller directory setup

--- express-tutorial/routes/people.js
+++ express-tutorial/routes/people.js
@@ -2,55 +2,32 @@ const express = require('express');
 // this is a router INSTANCE that takes care of the routing instead of the app
 const router = express.Router(); // invoked
 let { people } = require('../data');
+const {
+    getPeople,
+    createPerson,
+    createPersonPostman,
+    updatePerson,
+    deletePerson
+} = require('../controllers/people')
 
 // setting up base route
 
+// now we basically want to separate the functionality to the request to clean the code 
+// the pattern is MVC - model view controller - so we're putting this functionality in the controllers file
 
-router.get('/', (req, res) => {
-    res.status(200).json({success: true, data:people});
-});
+// FLAVOUR 1 
 
-router.post('/', (req, res) => {
-    const {name} = req.body; 
-    if(!name){
-        return res.status(400).json({success:false, msg: 'please provide name value'})
-    }
-    res.status(201).json({success: true, person: name})
-})
+router.get('/', getPeople);
+router.post('/', createPerson)
+router.post('/postman', createPersonPostman)
+router.put('/:id', updatePerson);
+router.delete('/:id', deletePerson)
 
-router.post('/postman', (req, res) => {
-    const { name } = req.body;
-    if(!name){
-        return res.status(400).json({success:false, msg: 'please provide name value'})
-    }
-    res.status(201).json({success: true, data: [...people, name]})
-})
+// FLAVOUR 2 
 
-
-router.put('/:id', (req, res) => {
-    const {id} = req.params;
-    const {name} = req.body;
-    const person = people.find((person) => person.id === Number(id));
-    if(!person){
-        return res.status(404).json({success:false, msg: `no person with id ${id}`})
-    }
-    const newPeople = people.map((person) => {
-        if(person.id === Number(id)){
-            person.name = name;
-        }
-        return person
-    })
-    res.status(200).json({success: true, data: newPeople});
-});
-
-router.delete('/:id', (req, res) => {
-    const person = people.find((person) => person.id === Number(req.params.id));
-    if(!person) {
-        return res.status(404).json({ success: false, msg: `no person with id ${req.params.id}`})
-    }
-    const newPeople = people.filter((person) => person.id !== Number(req.params.id));
-    return res.status(200).json({success: true, data: newPeople});
-})
+// router.route('/').get(getPeople).post(createPerson);
+// router.route('/postman').post(createPersonPostman);
+// router.route('/:id').put(updatePerson).delete(deletePerson)
 
 module.exports = router;
 // when we export the router, in the app.js we want to set up a app.use(for the path that starts with api/people, I want to use my people router)
