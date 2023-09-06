af259d8a22b8faa73b113b605d586704dc560b81
Matteus Gaarder <matteus.gaarder@gmail.com>
2023-09-06 20:10:21 +0100

    adds .findOneAndUpdate() functionality, timecode 02:16:10

--- task-manager/controllers/tasks.js
+++ task-manager/controllers/tasks.js
@@ -89,12 +89,32 @@ const getSingleTask = async (req, res) => {
 // Task.findOne({_id: taskID}) uses that taskID to find a task in the database with a matching _id.
 
 
-const updateTask = (req, res) => {
-    res.send('update task')
+
+const deleteTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params;
+        const task = await Task.findOneAndDelete( {_id: taskID} );
+        if(!task){
+            return res.status(404).json({msg: `No task with id: ${taskID}`});
+        }
+        res.status(200).json({ task });
+    } catch (error) {
+        res.status(500).json({ msg: error});
+    }
 };
 
-const deleteTask = (req, res) => {
-    res.send('delete task')
+const updateTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params;
+        const task = await Task.findOneAndUpdate({_id: taskID, }, req.body, { new: true, runValidators: true }); // timecode 02:13:22 for explanation about why we need to use an option as the third argument
+        if(!task){                                                            // basically because whilst this does update the task, it doesn't change the value in the get all request
+            return res.status(404).json({msg: `No task with id: ${taskID}`});
+        }
+        res.status(200).json({});
+
+    } catch (error) {
+        res.status(500).json({ msg: error}); 
+    }
 };
 
 module.exports = { getAllTasks, createNewTask, getSingleTask, updateTask, deleteTask }
